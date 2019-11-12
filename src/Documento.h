8eb91925fb049651f2867e6ff8e69740875cc2c6
Pedro Dias <pedrodias504@gmail.com>
2019-11-12 20:04:43 -0300

    Atualizada a estrutura de diretórios. Separados os arquivos de classe e melhoradas as estruturas de dados.

---
+++ src/Documento.h
@@ -0,0 +1,33 @@
+#ifndef DOCUMENTO_H
+#define DOCUMENTO_H
+#include <string>
+#include <fstream>
+#include <unordered_map>
+#include <vector>
+
+using std::string;
+using std::ifstream;
+using std::unordered_map;
+using std::vector;
+
+class Documento{
+    public:
+        //Inicializa o documento com nome "doc" e diretório "dir", preparando para a busca.
+        Documento(string doc,string dir);
+        //Retorna o nome do documento atual.
+        string nome();
+        //Retorna a frequência da palavra P no documento atual.
+        int freq(string P);
+        //Atribui ao documento atual as coordenadas contidas em V.
+        void set_coord(vector<double> V);
+        //Retorna as coordenadas do documento atual.
+        vector<double> coord();
+    private:
+        string nome_;
+        string dir_;
+        ifstream arquivo_;
+        unordered_map<string,int> tokens_;
+        vector<double> coord_;
+};
+
+#endif //DOCUMENTO_H
