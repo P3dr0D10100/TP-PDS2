d01b1d2b0716ab2f3c210aac90c3a80f6798c939
Pedro <pedrodias504@gmail.com>
2019-11-13 21:30:49 -0300

    Adicionado um destrutor para a classe Documento.

--- src/Documento.h
+++ src/Documento.h
@@ -12,16 +12,18 @@ using std::vector;
 
 class Documento{
     public:
-        //Inicializa o documento com nome "doc" e diretÃ³rio "dir", preparando para a busca.
+        //Inicializa o documento com nome "doc" e diretório "dir", preparando para a busca.
         Documento(string doc,string dir);
         //Retorna o nome do documento atual.
         string nome();
-        //Retorna a frequÃªncia da palavra P no documento atual.
+        //Retorna a frequência da palavra P no documento atual.
         int freq(string P);
         //Atribui ao documento atual as coordenadas contidas em V.
         void set_coord(vector<double> V);
         //Retorna as coordenadas do documento atual.
         vector<double> coord();
+        //Fecha o documento após o uso.
+        ~Documento();
     private:
         string nome_;
         string dir_;
