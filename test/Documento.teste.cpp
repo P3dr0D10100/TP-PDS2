939d7c0268f3f65be54dbfda0cc2d6b75d18ec1c
Pedro <pedrodias504@gmail.com>
2019-11-14 21:22:30 -0300

    Iniciada a escrita dos testes da classe Documento com a construção da classe Teste.

---
+++ test/Documento.teste.cpp
@@ -0,0 +1,28 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "include/doctest.h"
+#include "src/Documento.h"
+
+class Teste{
+    public:
+        static string nome(const Documento& doc)
+        {
+            return doc.nome_;
+        }
+        static string dir(const Documento& doc)
+        {
+            return  doc.dir_;
+        }
+        static int id(const Documento& doc)
+        {
+            return doc.id_;
+        }
+        static unordered_multiset<string> tokens(const Documento& doc)
+        {
+            return doc.tokens_;
+        }
+        static vector<double> coord(const Documento& doc)
+        {
+            return doc.coord_;
+        }
+};
+
