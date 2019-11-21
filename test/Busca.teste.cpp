eeb2d42c891454ba409f26bd261a1701aca084dd
Leonel Mota <leonelmota5@gmail.com>
2019-11-21 09:28:18 -0300

    teste id inv

--- test/Busca.teste.cpp
+++ test/Busca.teste.cpp
@@ -15,16 +15,13 @@ class Teste_MBus {
     static vector <Documento> documentos (const MBus &m){
         return m.docs_;
     }
-    static void calc_id (){
-        MBus::calcula_id_inv();
-    }
-
+    
 };
 
 TEST_SUITE("MBus"){
     TEST_CASE("ÍNDICE INVERTIDO E CONSTRUTOR"){
-        Documento um ("", "docs/doc.txt", 0);
-        Documento dois ("", "docs1/doc.txt", 0);
+        Documento um ("", "docs/doc4.txt", 0);
+        Documento dois ("", "docs1/doc5.txt", 1);
         vector <Documento> DOCS;
         DOCS.push_back(um);
         DOCS.push_back(dois);
@@ -33,51 +30,26 @@ TEST_SUITE("MBus"){
         CHECK (Teste_MBus::num_d(teste) == 2);
 
         map<string,set<int>> indice_teste {
+            make_pair("este", set<int> {0,1}),
+            make_pair("e", set<int> {0,1}),
             make_pair("um", set<int> {0,1}),
-            make_pair("arquivo", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
-            make_pair("um", set<int> {0}),
+            make_pair("documento", set<int> {0}),
+            make_pair("teste", set<int> {0,1}),
+            make_pair("de", set<int> {0}),
+            make_pair("com", set<int> {0,1}),
+            make_pair("dez", set<int> {0}),
+            make_pair("palavras", set<int> {0,1}),
+            make_pair("ok", set<int> {0}),
+        
         };
-        CHECK (Teste_MBus::indice() == indice_teste );
+        CHECK ( Teste_MBus::indice(teste) == indice_teste );
     }
 
-    TEST_CASE("Construtor"){
-        Documento um ("", "docs/doc.txt", 0);
-        vector <Documento>
-
+    TEST_CASE ("CALCULO DAS COORDENADAS"){
+        
     }
+
+   
 }
 
 
